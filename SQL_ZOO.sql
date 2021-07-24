69c9c43eeaf0eacb39d1a49fc6256f95ba1752ca
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2021-07-23 22:03:39 -0500

    commit 2: Ch2 part 1
    SELECT from WORLD Tutorial

--- SQL_ZOO.sql
+++ SQL_ZOO.sql
@@ -78,12 +78,44 @@ SELECT name, population/area
 
 
 
+#SELECT from WORLD Tutorial
 
 
 
+#1. Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+
+SELECT name, continent, population FROM world
 
 
 
+#2. How to use WHERE to filter records. Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
+
+SELECT name
+  FROM world
+ WHERE population >=200000000
+
+
+
+ #3. Give the name and the per capita GDP for those countries with a population of at least 200 million. HELP:How to calculate per capita GDP
+
+SELECT NAME, GDP/POPULATION FROM WORLD WHERE POPULATION > 200000000
+
+
+
+#4. Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 1000000 to get population in millions.
+
+SELECT NAME, POPULATION/1000000 FROM WORLD WHERE CONTINENT = 'SOUTH AMERICA'
+
+
+
+#5. Show the name and population for France, Germany, Italy
+
+SELECT name, population FROM world WHERE name IN ('France', 'Germany', 'Italy')
+
+
+#6. Show the countries which have a name that includes the word 'United'
+
+select name from world where name like '%united%'
 
 
 
