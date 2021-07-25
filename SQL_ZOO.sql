27c95cfe950326586a9d6125d4b41987e3a8a6ec
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2021-07-24 22:58:37 -0500

    commit 3: ch2 part 2
    SELECT from WORLD part 2 Tutorial

--- SQL_ZOO.sql
+++ SQL_ZOO.sql
@@ -12,7 +12,7 @@ SELECT name, population FROM world
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000
 
-#QUIZZ
+#QUIZZ 1
 
 #1. Select the code which produces this table
 #   name	    |population
@@ -119,7 +119,57 @@ select name from world where name like '%united%'
 
 
 
+#7. Two ways to be big: A country is big if it has an area of more than 3 million sq km or it has a population of more than 250 million. Show the countries that are big by area or big by population. Show name, population and area.
+select name,population, area from world where area > 3000000 or population > 250000000
 
+#8. Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area. Australia has a big area but a small population, it should be included. Indonesia has a big population but a small area, it should be included. China has a big population and big area, it should be excluded. United Kingdom has a small population and a small area, it should be excluded.
+select name, population, area from world where (area > 3000000 or population > 250000000) and (area < 3000000 or population < 250000000)
+
+
+
+#9. Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
+select name, round(population/1000000,2), round(gdp/1000000000,2) from world where continent = 'South America'
+
+
+
+
+#10. Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+select name, round(gdp/population,-3) from world where gdp >= 1000000000000
+
+
+#11. Greece has capital Athens. Each of the strings 'Greece', and 'Athens' has 6 characters. Show the name and capital where the name and the capital have the same number of characters. You can use the LENGTH function to find the number of characters in a string
+
+SELECT name, capital
+  FROM world
+ WHERE len(name) = len(capital)
+
+
+
+#12. The capital of Sweden is Stockholm. Both words start with the letter 'S'. Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word. You can use the function LEFT to isolate the first character. You can use <> as the NOT EQUALS operator.
+SELECT name, capital
+FROM world
+WHERE left(name,1) = left(capital,1) and name != capital
+
+
+
+#13.
+#Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in the name. They don't count because they have more than one word in the name.
+
+#Find the country that has all the vowels and no spaces in its name.
+
+#You can use the phrase name NOT LIKE '%a%' to exclude characters from your results.
+#The query shown misses countries like Bahamas and Belarus because they contain at least one 'a'
+
+
+SELECT name 
+   FROM world
+WHERE name LIKE '%a%' and name like '%e%' and name like '%i%' and name like '%o%' and name like '%u%'
+  AND name NOT LIKE '% %'
+
+
+
+
+#QUIZZ 2
 
 
 
