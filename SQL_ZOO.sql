5e17932d756e49d91c29a607b827cb6f8cfe03c8
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2021-08-09 16:53:48 -0500

    Commit 5: Ch4
    #SELECT within SELECT Tutorial

--- SQL_ZOO.sql
+++ SQL_ZOO.sql
@@ -388,6 +388,13 @@ Physics	    |  1
 
 
 
+#SELECT within SELECT Tutorial
+
+
+#1. List each country name where the population is larger than that of 'Russia'.
+#world(name, continent, area, population, gdp)
+
+
 
 
 
