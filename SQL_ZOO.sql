40011bd1c546724684c2edd42153f70ebcbab6de
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2021-09-03 23:19:15 -0500

    Update SQL_ZOO.sql

--- SQL_ZOO.sql
+++ SQL_ZOO.sql
@@ -394,6 +394,13 @@ Physics	    |  1
 #1. List each country name where the population is larger than that of 'Russia'.
 #world(name, continent, area, population, gdp)
 
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Romania')
+
+
+
 
 
 
