02eaa1123fad93646f814ae895261a7af65e52bf
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2021-12-06 23:02:24 -0600

    Continuing
    Continuation

--- SQL_ZOO.sql
+++ SQL_ZOO.sql
@@ -395,8 +395,47 @@ SELECT name FROM world
      (SELECT population FROM world
       WHERE name='Romania')
 
+
+
+
+
 #2. Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
 
+
+
+
+
+
+
+#3. List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+
+
+
+
+
+
+
+
+
+#4. Which country has a population that is more than Canada but less than Poland? Show the name and the population.
+
+
+
+
+
+
+
+
+
+#5. Germany (population 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany. Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+
+#The format should be Name, Percentage for example:
+
+#   name	   percentage
+#   Albania	   3%
+#   Andorra	   0%
+#   Austria	   11%
+#   ...	       ...
 
 
 
