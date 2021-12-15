18ba878111317b434b86bf60eae7322c944032ce
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2021-12-14 21:56:14 -0600

    Quick submission
    I am busy with a personal project

--- SQL_ZOO.sql
+++ SQL_ZOO.sql
@@ -457,6 +457,7 @@ SELECT name FROM world
 
 
 
+#10. Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
 
 
 
